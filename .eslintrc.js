f5adabae7438ba5d45c9fc83838a707ea76940a0
RThong <403216075@qq.com>
2022-12-09 01:41:36 +0800

    chore: 引入hooks eslint  添加alias

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,8 @@
 module.exports = {
-  extends: ["alloy", "alloy/react", "alloy/typescript"],
-  env: {
-    // Your environments (which contains several predefined global variables)
-    //
-    // browser: true,
-    // node: true,
-    // mocha: true,
-    // jest: true,
-    // jquery: true
-  },
-  globals: {
-    // Your global variables (setting to false means it's not allowed to be reassigned)
-    //
-    // myGlobal: false
-  },
+  extends: ['alloy', 'alloy/react', 'alloy/typescript'],
+  plugins: ['react-hooks'],
   rules: {
-    // Customize your rules
-  },
-};
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
+  }
+}
--- prettier.config.js
+++ prettier.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   plugins: [require("prettier-plugin-tailwindcss")],
+  semi: false,
+  singleQuote: true,
+  printWidth: 100,
+  trailingComma: "none",
+  arrowParens: "avoid",
+  endOfLine: "auto",
 };
